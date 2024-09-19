fe74f3e288dab41f864e6ae4941e72802468eef1
feragaia <ingana.felipe@gmail.com>
2024-09-19 16:08:48 -0600

    Prueba de yaml

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Cambiando el contenido de app.js
+          Cambiando el contenido de app.js;
+          Ahora con Yaml
         </p>
         <a
           className="App-link"
